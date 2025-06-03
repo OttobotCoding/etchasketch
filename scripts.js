787db1ead5179e38e4dc03df9dd11800133878d1
OttoBotCoding <msmigiel22@gmail.com>
2025-06-03 09:00:02 -0600

    got the grid working and hover operational still need to work on changing colors

--- scripts.js
+++ scripts.js
@@ -1,43 +1,70 @@
 const gridContainer = document.getElementById("grid-container");
-const numRows = 4;
-const numCols = 4;
-let children = gridContainer.childNodes;
-let gridItem = document.getElementsByClassName(".grid-item");
+let userValue = document.getElementById("button");
 
-for (let row = 0; row < numRows; row++) {
-    for (let col = 0; col < numCols; col++) {
-        const gridItem = document.createElement("div");
-        gridItem.classList.add("grid-item");
-        gridItem.textContent = "Box " + row + " - " + col;
-        gridContainer.appendChild(gridItem);
-    };
-};
-    
-for (let i = 0; i < gridItem.length; i++) {
-    gridItem[i].addEventListener('mouseover', function() {
-        gridItem[i].style.backgroundColor = 'red';
-    });
-};
-
-for (let i = 0; i < gridItem.length; i++) {
-    gridItem[i].addEventListener('mouseout', function() {
-        gridItem[i].style.backgroundColor = '#f0f0f0';
-    });
-};
-
-function createGrid() {
-    let squares = window.prompt('How many squares do you want in your grid?');
-    for (let i = children.length - 1; i >= 0; i --) {
-        gridContainer.removeChild(children[i]);
-    };
-    
-    for (let row = 0; row < squares; row++) {
-        for (let col = 0; col < squares; col++) {
-            const gridItem = document.createElement("div");
-            gridItem.classList.add("grid-item");
-            gridItem.textContent = "Box " + row + " - " + col;
-            gridContainer.appendChild(gridItem);
+function makeGrid() {
+    let number = window.prompt('How many squares do you want in your grid?');
+    if (number < 0 || number > 99 || isNaN(number)) {
+        console.log("No way bro!");
+    } else {
+    userValue.value = "";
+    gridContainer.innerHTML = "";
+    if (number < 0 || number > 99 || number == "") {
+        for (let i = 0; i < 10; i++) {
+            let row = document.createElement("div");
+            row.classList.add("row");
+            gridContainer.appendChild(row);
+            for (let k = 0; k < 10; k++) {
+                let col = document.createElement("div");
+                col.classList.add("col");
+                row.appendChild(col);
+                };
+                };
+            }   else {
+            for (let i = 0; i < number; i++) {
+                let row = document.createElement("div");
+                row.classList.add("row");
+                gridContainer.appendChild(row);
+                for (let k = 0; k < number; k++) {
+                    let col = document.createElement("div");
+                    col.classList.add("col");
+                    row.appendChild(col);
+                };
+            };
         };
     };
+    draw();
 };
+
+function draw() {
+    let columns = document.getElementsByClassName("col");
+    for (let i = 0; i < columns.length; i++) {
+    columns[i].addEventListener("mouseover", function() {
+        columns[i].style.backgroundColor = 'red';
+    });
+};
+
+    for (let i = 0; i < columns.length; i++) {
+        columns[i].addEventListener('mouseout', function() {
+            columns[i].style.backgroundColor = '#f0f0f0';
+        });
+    };
+};
+
+// my attempt before researching //
+
+// function createGrid() {
+//     let squares = window.prompt('How many squares do you want in your grid?');
+//     for (let i = children.length - 1; i >= 0; i --) {
+//         gridContainer.removeChild(children[i]);
+//     };
+    
+//     for (let row = 0; row < squares; row++) {
+//         for (let col = 0; col < squares; col++) {
+//             const gridItem = document.createElement("div");
+//             gridItem.classList.add("grid-item");
+//             gridItem.textContent = "Box " + row + " - " + col;
+//             gridContainer.appendChild(gridItem);
+//         };
+//     };
+// };
 
