97a4323110f5c164be596a42b05e105d2ff2313d
OttobotCoding <139722481+OttobotCoding@users.noreply.github.com>
2025-06-03 05:43:20 -0600

    added button to determine number of boxes still need to fix hover coloring and even display

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const gridContainer = document.getElementById("grid-container");
 const numRows = 4;
 const numCols = 4;
+let children = gridContainer.childNodes;
+let gridItem = document.getElementsByClassName(".grid-item");
 
 for (let row = 0; row < numRows; row++) {
     for (let col = 0; col < numCols; col++) {
@@ -8,11 +10,9 @@ for (let row = 0; row < numRows; row++) {
         gridItem.classList.add("grid-item");
         gridItem.textContent = "Box " + row + " - " + col;
         gridContainer.appendChild(gridItem);
-    }
-}
-
-const gridItem = document.getElementsByClassName('grid-item');
-
+    };
+};
+    
 for (let i = 0; i < gridItem.length; i++) {
     gridItem[i].addEventListener('mouseover', function() {
         gridItem[i].style.backgroundColor = 'red';
@@ -24,3 +24,20 @@ for (let i = 0; i < gridItem.length; i++) {
         gridItem[i].style.backgroundColor = '#f0f0f0';
     });
 };
+
+function createGrid() {
+    let squares = window.prompt('How many squares do you want in your grid?');
+    for (let i = children.length - 1; i >= 0; i --) {
+        gridContainer.removeChild(children[i]);
+    };
+    
+    for (let row = 0; row < squares; row++) {
+        for (let col = 0; col < squares; col++) {
+            const gridItem = document.createElement("div");
+            gridItem.classList.add("grid-item");
+            gridItem.textContent = "Box " + row + " - " + col;
+            gridContainer.appendChild(gridItem);
+        };
+    };
+};
+
